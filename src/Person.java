3df9cbeca6d9cf8e54082be40f77dab12df3c657
mimya2008 <shantahafiz.mimya@gmail.com>
2025-03-12 00:14:01 -0230

    Student, children  of person with some update

---
+++ src/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+    protected String myName; // name of the person
+    protected int myAge;   // person’s age
+    protected String myGender;   // “M” for male, “F” for female
+
+
+    // constructors
+    public Person(String name, int age, String gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    // getters setters
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+
+    public int getMyAge() {
+        return myAge;
+    }
+
+    public void setMyAge(int myAge) {
+        this.myAge = myAge;
+    }
+
+    public String getMyGender() {
+        return myGender;
+    }
+
+    public void setMyGender(String myGender) {
+        this.myGender = myGender;
+    }
+
+    // to String
+    public String toString() {
+        return myName + ", age: " + myAge + ", gender: " + myGender;
+    }
+}
